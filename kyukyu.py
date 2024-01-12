0caf614701559c4ca74a9e5f642ecf1fa3b1f2f3
LimSH <lsh00731@gmail.com>
2024-01-12 17:18:22 +0900

    refactor: built kyukyu in main func

--- kyukyu.py
+++ kyukyu.py
@@ -1,9 +1,5 @@
-def do_kyukyu(n:int):
-    for i in range(1,9+1):
-        print(f'{n} X {i} = {n*i}')
-
-
 if __name__=='__main__':
     n = int(input('please enter number(1~9): '))
-    do_kyukyu(n)
+    for i in range(1,9+1):
+        print(f'{n} X {i} = {n*i}')
 
