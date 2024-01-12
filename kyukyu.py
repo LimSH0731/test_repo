3ffe0c17de8cded1e853fcf08b4cd0ddb845b77d
LimSH <lsh00731@gmail.com>
2024-01-12 17:13:52 +0900

    feat: add feature - print fomula and print input messege

--- kyukyu.py
+++ kyukyu.py
@@ -1,9 +1,9 @@
 def do_kyukyu(n:int):
     for i in range(1,9+1):
-        print(f'{n*i}')
+        print(f'{i} X {n} = {n*i}')
 
 
 if __name__=='__main__':
-    n = int(input())
+    n = int(input('please enter number(1~9): '))
     do_kyukyu(n)
 
